99abb8ada64c683b16c21f63f106b8c692c2a3f2
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-05-18 19:24:05 -0400

    Created three tables in the employees.sql file.

--- employees.sql
+++ employees.sql
@@ -2,3 +2,23 @@ DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB;
 
 USE employees_DB;
+
+CREATE TABLE department (
+    dept_name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE position (
+    title VARCHAR(30) NULL,
+    salary DECIMAL,
+    department_id INT(10),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT(10),
+    manager_id INT(10),
+    PRIMARY KEY (id)
+)
