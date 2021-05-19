08e112bafcf716921210f9987d797168dc1c5969
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-05-18 21:01:22 -0400

    Created function to initiate the app.

--- tracker.js
+++ tracker.js
@@ -1,2 +1,72 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 8080,
+
+    user: 'root',
+
+    password: 'Dizzle80!',
+    database: 'employees_DB',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    runApp();
+});
+
+const runApp = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees.',
+                'View Employees by Department.',
+                'View Employees by Manager.',
+                'Add Employee.',
+                'Remove Employee',
+                'Update Employee Role',
+                'Update Employee Manager.'
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case 'View All Employees.':
+                    allEmployees();
+                    break;
+
+                case 'View Employees by Department.':
+                    byDept();
+                    break;
+
+                case 'View Employees by Manager.':
+                    byManager();
+                    break;
+
+                case 'Add Employee.':
+                    addEmployee();
+                    break;
+
+                case 'Remove Employee':
+                    removeEmployee();
+                    break;
+
+                case 'Update Employee Role':
+                    updateEmployee();
+                    break;
+
+                case 'Update Employee Manager.':
+                    updateManager();
+                    break;
+
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+        });
+};
+
